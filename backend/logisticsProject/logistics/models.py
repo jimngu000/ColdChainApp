6ed97a69a3aabb5c7efe414f1342e145065daa12
Hans Easton <hanreaston@gmail.com>
2024-05-06 11:45:13 +0800

    Formalize models.py.

--- backend/logisticsProject/logistics/models.py
+++ backend/logisticsProject/logistics/models.py
@@ -1,22 +1,42 @@
 from django.db import models
 
+# USER
+class User(models.Model):
+
+
 class District(models.Model):
     name = models.CharField(max_length=200)
 
 class Hospital(models.Model):
     name = models.CharField(max_length=200)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+
+# TODO Refrigerator model
+class Refrigerator(models.Model):
+   hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+
     
 class Vaccine(models.Model):
     name = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
     producer = models.CharField(max_length=200)
-    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    value = models.CharField(max_length=200)
+    refrigerator = models.ForeignKey(Refrigerator, on_delete=models.CASCADE)
 
 class Log(models.Model):
+
+    # TODO: this should also be a reference to a user model, which is not defined yet.
     user = models.CharField(max_length=200)
+
+    # references
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    refrigerator = models.ForeignKey(Refrigerator, on_delete=models.CASCADE)
+    vaccine = models.ForeignKey(Vaccine, on_delete=models.CASCADE)
+
+    # TODO: previous and updated value.
     value = models.IntegerField(default=0)
+
     timestamp = models.DateTimeField(auto_now_add=True)
 
