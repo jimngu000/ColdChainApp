ba7b2a1b7b8e2eb80023c60ac841ae623191c469
Hans Easton <hanreaston@gmail.com>
2024-05-13 02:12:48 +0800

    Complete script for populating mock data into the backend.

--- backend/logisticsProject/logistics/models.py
+++ backend/logisticsProject/logistics/models.py
@@ -9,18 +9,18 @@ class User(models.Model):
 # District model
 class District(models.Model):
     name = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 # Hospital model
 class Hospital(models.Model):
     name = models.CharField(max_length=200)
-    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, null=True)
 
 # Refrigerator model
 # Note: For demo purposes, only taking a subset of fields
 # from the the doc shared by Professor Anderson. 
 class Refrigerator(models.Model):
-   hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+   name = models.CharField(max_length=200)
    model_id = models.CharField(max_length=200)
    manufacturer = models.CharField(max_length=200)
    temp_monitor_installed = models.BooleanField(default=False)
@@ -29,6 +29,7 @@ class Refrigerator(models.Model):
    voltage_regulator_installed =  models.BooleanField(default=False)
    regulator_type = models.CharField(max_length=200)
    vaccine_count = models.IntegerField(default=0)
+   hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, null=True)
 
 # Log model
 class Log(models.Model):
@@ -37,7 +38,7 @@ class Log(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
-    refrigerator = models.ForeignKey(Refrigerator, on_delete=models.CASCADE)
+    refrigerator = models.ForeignKey(Refrigerator, on_delete=models.CASCADE, null=True)
 
     # which field has been updated
     field_name = models.CharField(max_length=200)
