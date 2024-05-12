e6bed4aae7ae14176dae1e657d9e54c29d2694fc
Hans Easton <hanreaston@gmail.com>
2024-05-12 15:43:38 +0800

    Update user model.

--- backend/logisticsProject/logistics/models.py
+++ backend/logisticsProject/logistics/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 
-# USER
 class User(models.Model):
-
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
 
 class District(models.Model):
     name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Hospital(models.Model):
     name = models.CharField(max_length=200)
     district = models.ForeignKey(District, on_delete=models.CASCADE)
 
-
 # TODO Refrigerator model
 class Refrigerator(models.Model):
    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
 
-    
 class Vaccine(models.Model):
     name = models.CharField(max_length=200)
     type = models.CharField(max_length=200)
